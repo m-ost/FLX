5ea0af9d4f17b7e871672695aae4a036f1d64b63
Mariya Ostash <46262118+m-ost@users.noreply.github.com>
2019-02-05 14:55:35 +0200

    Update task2.js

--- FLX_homework_6/homework/js/task2.js
+++ FLX_homework_6/homework/js/task2.js
@@ -1,10 +1,10 @@
-var amount = prompt('Please enter an amount:');
-var discount = prompt('Please enter a discount:');
-if (amount >= 0 && discount >= 0 && amount <= 9999999 && discount <= 99) {
-    var price = Math.floor((amount - amount * discount / 100) * 100) / 100;
-    var difference = Math.floor((amount - price) * 100) / 100;
-    alert('Price without discount: ' + amount + '\n' + 'Discount: ' + discount + '\n' +
-        'Price with discount: ' + price + '\n' + 'Saved: ' + difference);
+var amount = Number(prompt('Please enter an amount:'));
+var discount = Number(prompt('Please enter a discount:'));
+if(amount >= 0 && discount >= 0 && amount <= 9999999 && discount <= 99){
+  var price = parseFloat((amount - amount * discount / 100).toFixed(2));
+  var difference = parseFloat((amount - price).toFixed(2));
+  alert('Price without discount: '+amount+'\n'+'Discount: '+discount+'\n'+
+  'Price with discount: '+price+'\n'+'Saved: '+difference);
 } else {
-    alert('Invalid input data');
+  alert('Invalid input data');
 }
