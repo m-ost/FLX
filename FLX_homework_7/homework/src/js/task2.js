4636a4c99b27a4b274c21f6ee37952cf2dbda7b3
Mariya Ostash <46262118+m-ost@users.noreply.github.com>
2019-02-10 22:16:22 +0200

    Update task2.js

--- FLX_homework_7/homework/src/js/task2.js
+++ FLX_homework_7/homework/src/js/task2.js
@@ -5,7 +5,6 @@ let maxPrize = 10;
 let defaultPrize = 10;
 let gameIsOn = true;
 let attempts = 3;
-
 if (!confirm('Do you want to play a game?')) {
   alert(`You did not become a millionaire, but can.`);
 } else {
@@ -19,36 +18,34 @@ if (!confirm('Do you want to play a game?')) {
         Total prize: ${totalPrize}$
         Possible prize on current attempt: ${maxPrize}$`
       ));
-
       if (!userNumber) {
         break;
       }
-
       if (userNumber === randomNumber) {
         guessed = true;
         totalPrize = maxPrize;
         gameIsOn = confirm(`
-          Congratulations! Your prize is ${totalPrize}$.Do you want to continue?
+        Congratulations! Your prize is ${totalPrize}$.Do you want to continue?
         `);
-        if(gameIsOn){
+          if(gameIsOn){
             defaultPrize = defaultPrize * 3;
             maxPrize = defaultPrize;
             maxRandom *= 2;
             finalPrize = maxPrize + totalPrize;
             break;
           }
-            if (!gameIsOn) {
-              gameIsOn = alert(`Thank you for a game. Your prize is: ${finalPrize}$.`);
-              gameIsOn = confirm(`Do you want to play again?`);
-              totalPrize = 0;
-              maxPrize = 10;
-              maxRandom = 5;
-            }
+          if (!gameIsOn) {
+             gameIsOn = alert(`Thank you for a game. Your prize is: ${finalPrize}$.`);
+             gameIsOn = confirm(`Do you want to play again?`);
+             totalPrize = 0;
+             maxPrize = 10;
+             maxRandom = 5;
+          }
         } else {
           guessed = false;
           maxPrize = Math.floor(maxPrize/2);
-      }
-  }
+        }
+    }
 
     if (!guessed) {
       gameIsOn = alert(`Thank you for a game. Your prize is: ${totalPrize}$.`);
