8fca30da0e475fdbc527c52fc66c0fc795faad73
Mariya Ostash <46262118+m-ost@users.noreply.github.com>
2019-03-26 14:43:46 +0200

    Inheritance

---
+++ FLX_homework_16/homework/js/app.js
@@ -0,0 +1,112 @@
+//Task-1
+function assign(targetObj) {
+  for(var i = 1; i < arguments.length; i++) {
+    Object.keys(arguments[i]).forEach((key) => {
+      targetObj[key] = arguments[i][key];
+    });
+  }
+
+  return targetObj;
+};
+
+// var defaults = { a: 123, b: 777 };
+// var options = { a: 456 };
+// var configs = assign({}, defaults, options);
+
+//Task - 2
+function Bot(obj) {
+  this.name = obj.name;
+  let speed = obj.speed;
+  this.defaultSpeed = obj.speed;
+  this.x = obj.x;
+  this.y = obj.y;
+}
+
+Bot.prototype.getSpeed = function() {
+  return this.speed;
+};
+
+Bot.prototype.setSpeed = function(speed) {
+  this.speed = speed;
+};
+
+Bot.prototype.getDefaultSpeed = function() {
+  return this.defaultSpeed;
+};
+
+Bot.prototype.getCoordinates = function () {
+  return {x: this.x, y: this.y};
+};
+
+Bot.prototype.setCoordinates = function(x,y) {
+  this.x = x;
+  this.y = y;
+};
+
+Bot.prototype.move = function(direction) {
+  this.changeCoords(direction);
+};
+
+Bot.prototype.changeCoords = function(direction) {
+  switch(direction) {
+    case 'down':
+    this.x += this.speed;
+    break;
+    case 'up':
+    this.x += this.speed;
+    break;
+    case 'right':
+    this.y += this.speed;
+    break;
+    case 'left':
+    this.y -= this.speed;
+    default:
+    console.log(`Oops! I know only basic moves. Please, choose the one from 'up/down/left/right' options.`);
+  }
+};
+
+Bot.prototype.showPosition = function() {
+  console.log(`{this.name} coordinates are: x: ${this.x}; y: ${this.y}.`);
+};
+
+function Racebot(obj) {
+  Bot.call(this, obj);
+  this.previousMove = null;
+}
+
+Racebot.prototype = Object.create(Bot.prototype);
+Racebot.prototype.constructor = Racebot;
+
+Racebot.prototype.move = function(direction) {
+  if(direction === this.previousMove) {
+    this.speed++;
+  } else {
+    this.speed = this.defaultSpeed;
+    this.previousMove = direction;
+    this.changeCoords(direction);
+  }
+};
+
+function Speedbot(obj) {
+  Bot.call(this, obj);
+}
+
+Speedbot.prototype = Object.create(Bot.prototype);
+Speedbot.constructor = Speedbot;
+
+Speedbot.prototype.prepareEngine = function() {
+  this.speed += 2;
+};
+
+Speedbot.prototype.move = function(direction) {
+  this.changeCoords(direction);
+  if(direction) {
+    if(this.speed != this.defaultSpeed) {
+      this.speed--;
+    }
+  }
+};
+
+// let Botty = new Bot({name: "Betty", speed: 2, x: 0, y: 1});
+// let Racy = new Racebot({name:'Racy', speed: 4, x: 2, y:6});
+// let Speedy = new Speedbot({name:"Spe", speed:3, x:5, y:2});
